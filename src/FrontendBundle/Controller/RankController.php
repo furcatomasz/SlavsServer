6994411808ed10bf66e8345f3efd56c4f970ef10
tomekDom <thron@knights.pl>
2018-06-07 17:29:59 +0200

    #master  * update bootstrap  * fixed styles

---
+++ src/FrontendBundle/Controller/RankController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace FrontendBundle\Controller;
+
+use AppBundle\Manager\PlayerManager;
+use FOS\UserBundle\Controller\SecurityController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
+
+class RankController extends SecurityController
+{
+
+    /**
+     * @var PlayerManager
+     *
+     * @DI\Inject("manager.player")
+     */
+    public $playerManager;
+
+    /**
+     * @Route("/rank/players", name="rank_players")
+
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function playersAction(Request $request)
+    {
+        $players = $this->playerManager->getRepo()->findAll();
+
+        return $this->render(
+            'FrontendBundle:rank:players.html.twig',
+            [
+                'players' => $players,
+            ]
+        );
+
+    }
+
+
+}
